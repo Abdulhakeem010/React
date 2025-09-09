f5063389f26ec5305e131df0db58bfabc1dab757
abdulhakeem010 <bigmuak010@gmail.com>
2025-09-09 16:40:27 +0100

    Add advice fetching functionality to app component

--- my-app/src/app.jsx
+++ my-app/src/app.jsx
@@ -1,10 +1,20 @@
 export default function app() {
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>Hello World!</h1>
-      <button>Get advice</button>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
 }
 
-// https://api.adviceslip.com/advice
+// function getCount() {
+//   return <p>"You have read {count} advice"</p>;
+// }
